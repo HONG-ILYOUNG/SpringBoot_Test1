ef3ba62740d3d7828d300c361dd28cad8aa484e6
Hong Il Young <cavrena@gmail.com>
2020-12-16 10:57:45 +0900

    config 추가

---
+++ src/main/java/com/example/demo/config/ElasticsearchConfig.java
@@ -0,0 +1,32 @@
+@Configuration
+@PropertySource(value = "classpath:applications.properties")
+@EnableElasticsearchRepositories(basePackages = "com.example.demo.repository")
+public class ElasticsearchConfig {
+	
+	@Value("${elasticsearch.host}")
+    private String EsHost;
+
+    @Value("${elasticsearch.port}")
+    private int EsPort;
+
+    @Value("${elasticsearch.clustername}")
+    private String EsClusterName;
+
+    @Bean
+    public Client client() throws Exception {
+        Settings esSettings = Settings.settingsBuilder()
+                .put("cluster.name", EsClusterName)
+                .build();
+
+        return TransportClient.builder()
+                .settings(esSettings)
+                .build()
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(EsHost), EsPort));
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
+        return new ElasticsearchTemplate(client());
+    }
+
+}
